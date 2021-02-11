fb61a601efe983a8ef68c13a2c2aa75f298ef053
EthanDevs404 <63881996+EthanDevs404@users.noreply.github.com>
2021-02-11 00:18:52 +0000

    Initial Commit

---
+++ react-client/src/App.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Deutsche Bank Project</h1>
+        <p>The basic file structure has been setup; but feel free to change it as you wish.</p>
+        <p>This can be run in docker etc and part of docker compose; but just for testing - I recommend
+        simply running it on your machine for now: `npm install` to get node modules then `npm start`.</p>
+    </div>
+  );
+}
+
+export default App;
