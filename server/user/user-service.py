ffc33bc5afc158210eee3353f33a8124a7bd4f18
EthanDevs404 <63881996+EthanDevs404@users.noreply.github.com>
2021-02-16 23:22:55 +0000

    user-events database setup

--- server/user/user-service.py
+++ server/user/user-service.py
@@ -1,15 +1,43 @@
 from flask import Flask
+import mysql.connector
+import json
+
 app = Flask(__name__)
 
-# BASIC API: Don't change the main function for now; as currently, it exposes 5001 in the DockerFile, and the compose and nginx config makes use of this port.
+def test_database():
+     config = {
+                    'user': 'root',
+                    'password': 'root',
+                    'host': 'user-event-db',
+                    'port': '3306',
+                    'database': 'userEvent'
+                }
+     connection = mysql.connector.connect(**config)
 
+     cursor = connection.cursor()
+     cursor.execute('SELECT * FROM Users')
+
+     results = []
+     for row in cursor:
+         results.append(row)
+
+     cursor.close()
+     connection.close()
+
+     return results
+
+
+# Basic example of an api route (called front-end from localhost/user)
 @app.route('/')
 def hello_world():
     return {'APIUser' : 'Active'}
 
+# Example API call; using the database:
 @app.route('/test')
 def test():
-    return {'API: User Test' : 'Passed!'}
+     return json.dumps({'users': test_database()})
 
+
+# Running the API:
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
